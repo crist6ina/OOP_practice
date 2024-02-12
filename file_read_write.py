f670da0d849c7186c6212bb8b05bfbe958efd888
crist6ina <s.cristinamariax@gmail.com>
2024-02-12 19:31:02 +0100

    initial commit

---
+++ file_read_write.py
@@ -0,0 +1,115 @@
+import sys
+import os
+from pathlib import Path
+
+
+class FileTypeError:
+    pass
+
+
+class FileExtensionError:
+    pass
+
+
+class FileWriter:
+    def __init__(self, filename: str):
+        """FileWriter constructor"""
+        self.filename = filename
+        if not isinstance(self.filename, str):
+            raise FileTypeError
+        self.parent_path = str(Path(__file__).parent)
+        self.path = self.parent_path + '/' + self.filename
+        self.parent = self.parent_path[self.parent_path.rindex('/') + 1::]
+
+    def write_to_file(self, data: str, mode='a'):
+        """Write data to file"""
+        if not isinstance(data, str):
+            raise ValueError('Content must be of str type')
+
+        with open(self.filename, mode) as fw:
+            fw.write(f'{data}\n')
+
+    def writer(self, content: str, mode='a'):
+        """Write or append data to file"""
+        if not isinstance(content, str):
+            raise ValueError
+
+        if os.path.exists(self.path) and mode == 'w':
+            press = input(f'{self.filename} is an existing file in {self.parent} directory. '
+                          f'Are you sure you want to overwrite?(yes/append/no) ')
+            if press == 'yes':
+                self.write_to_file(content, 'w')
+            elif press == 'append':
+                self.write_to_file(content, 'a')
+            else:
+                sys.exit(0)
+        elif not os.path.exists(self.path) and mode == 'a':
+            print(f'{self.filename} does not exist in {self.parent} directory. New file will be created.')
+            self.write_to_file(content)
+        else:
+            self.write_to_file(content)
+
+    @property
+    def size(self):
+        """Return file size"""
+        try:
+            return os.path.getsize(self.filename)
+        except FileNotFoundError:
+            print(f'Cannot get size because {self.filename} was not found')
+            sys.exit(2)
+
+    def delete_file(self):
+        """Delete file from directory if it exists."""
+        if os.path.exists(self.path):
+            press = input(f'This action will completely remove {self.filename}. Do you want to proceed?(yes/no) ')
+            if press == 'yes':
+                os.remove(self.path)
+            else:
+                sys.exit(0)
+        else:
+            print(f'{self.filename} does not exist in {self.parent}')
+
+    @classmethod
+    def from_file(cls, file: str) -> list:
+        """Alternative constructor"""
+        if not os.path.exists(file):
+            raise FileNotFoundError(f'The file does not exist')
+        file_objects = []
+        with open(file, 'r') as fr:
+            content = fr.readlines()
+
+        for file_name in content:
+            file_objects.append(cls(file_name.strip()))
+        return file_objects
+
+    def __str__(self):
+        return self.filename
+
+
+class FileReader(FileWriter):
+    def __init__(self, filename: str):
+        """FileReader constructor"""
+        super().__init__(filename)
+
+    def reader(self):
+        """Return content of given file"""
+        try:
+            with open(self.path, 'r') as fread:
+                content = fread.read()
+                return content
+        except FileNotFoundError:
+            sys.exit('File not found')
+
+
+if __name__ == '__main__':
+    # tester_file = input('Choose file to manipulate: ')
+    # tester = ManipulateFile(tester_file)
+    try:
+        text_file = FileWriter('dummy2.txt')
+        text_file.writer('nu')
+        reader_objects = FileReader.from_file('files.txt')
+        object_list = [obj.filename for obj in reader_objects]
+        print(text_file)
+        print(object_list)
+    except ValueError:
+        print('Value error!')
---
+++ ses22hw.py
@@ -0,0 +1,80 @@
+"""Interview question - ReadEquipment class, allows the reading and processing of network equipment data"""
+import pandas as pd
+import os.path
+
+
+class ReadEquipment:
+    """ReadEquipment class
+    Objects of this class may be created based on a .txt file containing specific network equipment data."""
+
+    def __init__(self, file: str):
+        """ReadEquipment constructor"""
+        self.file = file
+        self.network_element_name = ReadEquipment.readfile(self).split()[1]
+
+    def readfile(self) -> str:
+        """Return txt file content in a string."""
+        if os.path.exists(self.file):
+            with open(self.file, 'r') as fr:
+                content = fr.read()
+                return content
+        else:
+            raise FileNotFoundError
+
+    def __split_content(self, row: int = None) -> list[list[str]] or str:
+        """Return a list of lists, where each list represents a row and has each word as a separate element.
+        If a specific row was specified, the row as a string will be returned"""
+        row_list = self.readfile().split('\n')
+        if row:
+            return row_list[row]
+        word_list = [row.split() for row in row_list if row.split()]
+        return word_list
+
+    @property
+    def result_of_operation(self) -> str:
+        """Checks if operation was successful and returns said result."""
+        content = self.__split_content()[3]
+        return ' '.join(content[-2::])
+
+    def file_content_to_dataframe(self):
+        """Creates dataframe object from given file where the columns are represented by
+        Local Cell ID(index), Cell Name, Column_2, Column_3 specific to network element."""
+        # actual columns as lists
+        config_list = self.__split_content()[7:-2]
+
+        # list with header names
+        raw_df_header = self.__split_content(7).split('  ')
+        df_header = [header.strip() for header in raw_df_header if header.strip()]
+
+        # creating dataframe
+        dataframe = pd.DataFrame(config_list, columns=df_header)
+        dataframe = dataframe.set_index('Local Cell ID')
+        return dataframe
+
+    def get_column_by_name(self, column: str, local_cell_id: str = '') -> str:
+        """Return entire column of dataframe by giving column name as argument.
+        A specific row of the column may be returned if local_cell_id index is provided."""
+        dataframe = self.file_content_to_dataframe()
+        if local_cell_id:
+            if local_cell_id not in dataframe[column]:
+                raise ValueError('Invalid Local Cell ID.')
+            return f'{column}: id {local_cell_id} -> {dataframe[column][local_cell_id]}'
+        return dataframe[column]
+
+    def __str__(self):
+        return f'ReadEquipment object [{self.network_element_name}] with status: {self.result_of_operation}'
+
+
+if __name__ == '__main__':
+    try:
+        eq = ReadEquipment('network_element_data.txt')
+        print(eq.network_element_name)
+        print(eq.result_of_operation)
+        data = eq.file_content_to_dataframe()
+        print(data)
+        column_3 = eq.get_column_by_name('Column_3', '2')
+        print(column_3)
+    except FileNotFoundError:
+        print('File not found.')
+    except ValueError as e:
+        print(e)
